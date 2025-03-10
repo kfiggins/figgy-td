6af925b5f24147a8c054dfc640a8bb420e93ab8c
Kyler Figgins <kyler.figgins@gmail.com>
2025-03-09 22:37:44 -0400

    Add a basic enemy moving

--- src/screens/game/index.js
+++ src/screens/game/index.js
@@ -1,10 +1,55 @@
-const drawScreen = (ctx) => {
+const initialGameState = {
+  enemy: {
+    x: 800,
+    y: 150,
+    width: 30,
+    height: 30,
+    speed: 100, // pixels per second
+  },
+};
+
+const drawScreen = (ctx, gameState) => {
   ctx.fillStyle = "#12263A";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
   ctx.fillStyle = "white";
   ctx.font = "40px Arial";
   ctx.fillText("THIS IS THE GAME", 200, 150);
+
+  // Draw enemy
+  ctx.fillStyle = "red";
+  ctx.fillRect(gameState.enemy.x, gameState.enemy.y, gameState.enemy.width, gameState.enemy.height);
 };
 
-export default { drawScreen };
+const update = ({ ctx, gameState, deltaTime }) => {
+  const secondsPassed = deltaTime / 1000;
+  const enemyDistanceMoved = gameState.enemy.speed * secondsPassed;
+
+  const newEnemy = {
+    ...gameState.enemy,
+    x: gameState.enemy.x - enemyDistanceMoved,
+  };
+
+  // If enemy moves off screen, reset position to right side
+  const resetEnemy = newEnemy.x < -newEnemy.width ? { ...newEnemy, x: ctx.canvas.width } : newEnemy;
+
+  return {
+    ...gameState,
+    enemy: resetEnemy,
+  };
+};
+
+let gameState = initialGameState;
+
+export default {
+  drawScreen: (ctx) => drawScreen(ctx, gameState),
+  update: ({ deltaTime, ctx }) => {
+    gameState = update({ ctx, deltaTime, gameState });
+    return gameState;
+  },
+  getState: () => gameState,
+  resetState: () => {
+    gameState = initialGameState;
+    return gameState;
+  },
+};
