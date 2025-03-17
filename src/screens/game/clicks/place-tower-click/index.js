1f30aff97b4e63471460fff4d02310f6202c68fc
Kyler Figgins <kyler.figgins@gmail.com>
2025-03-16 22:31:29 -0400

    Add economy

--- src/screens/game/clicks/place-tower-click/index.js
+++ src/screens/game/clicks/place-tower-click/index.js
@@ -2,13 +2,19 @@ export default (ctx) => {
   const { x, y, state } = ctx;
   const { tiles, isOffBoard, getGridPosition, getCoordinates, startLocation, endLocation, findPath } = state.board;
 
-  if (isOffBoard(x, y)) return;
+  // TODO: Cost should come from tower type being placed when ready.
+  const TOWER_COST = 10;
+  if (state.player.gold < TOWER_COST) {
+    return ctx;
+  }
+
+  if (isOffBoard(x, y)) return ctx;
 
   const { row, col } = getGridPosition(x, y);
 
   const oldTile = tiles[row][col];
   if (!oldTile || oldTile.type !== "tile") {
-    return;
+    return ctx;
   }
   tiles[row][col] = {
     type: "tower",
@@ -22,15 +28,20 @@ export default (ctx) => {
     range: 200,
   };
 
-  const startCoordinates = getCoordinates(startLocation.row, startLocation.col)
-  const endCoordinates = getCoordinates(endLocation.row, endLocation.col)
-  const path = findPath(tiles, startCoordinates.x, startCoordinates.y, endCoordinates.x, endCoordinates.y)
+  const startCoordinates = getCoordinates(startLocation.row, startLocation.col);
+  const endCoordinates = getCoordinates(endLocation.row, endLocation.col);
+  const path = findPath(tiles, startCoordinates.x, startCoordinates.y, endCoordinates.x, endCoordinates.y);
   if (path.length === 0) {
     tiles[row][col] = oldTile;
-    return;
+    return ctx;
   }
 
-  const newState = { ...state, newTowerPlaced: true, board: { ...state.board, tiles: tiles } };
+  const newState = {
+    ...state,
+    newTowerPlaced: true,
+    board: { ...state.board, tiles: tiles },
+    player: { ...state.player, gold: state.player.gold - TOWER_COST },
+  };
 
   return { ...ctx, state: newState };
 };
